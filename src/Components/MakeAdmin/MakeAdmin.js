5916984d243e255e608dcf194bb90571b0a1ab43
alikhanmohammadsajid <alikhanmohammadsajid@gmail.com>
2021-04-18 23:31:08 +0600

    last commit

--- src/Components/MakeAdmin/MakeAdmin.js
+++ src/Components/MakeAdmin/MakeAdmin.js
@@ -4,8 +4,19 @@ import { useForm } from "react-hook-form";
 const MakeAdmin = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => {
-        
-        console.log(data)};
+        const adminData = {
+            email: data.email
+        }
+        const url = `http://localhost:5000/addAdmin`
+        console.log(adminData)
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },body: JSON.stringify(adminData)
+        })
+        .then(res => console.log('server site', res))
+    };
     return (
         <div className="row">
             <div className="col-md-4">
@@ -13,15 +24,16 @@ const MakeAdmin = () => {
             </div>
             <div className="col-md-8 mt-5">
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <input style={{width:'500px',
-                     borderRadius: "50px",
-                     height:'50px'
-                
-                }} className="mb-5 text-center" type="text" name="email" placeholder="@gmail.com" {...register("email")} />
-                    {errors.exampleRequired && <span>This field is required</span>}
-                    <br/>
+                    <input style={{
+                        width: '500px',
+                        borderRadius: "50px",
+                        height: '50px'
 
-                    <input className="ms-5 mt-5 btn btn-warning"type="submit" />
+                    }} className="mb-5 text-center" type="text" name="email" placeholder="@gmail.com" {...register("email")} />
+                    {errors.exampleRequired && <span>This field is required</span>}
+                    <br />
+
+                    <input className="ms-5 mt-5 btn btn-warning" type="submit" />
                 </form>
             </div>
         </div>
